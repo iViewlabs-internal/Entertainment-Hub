fba9592b2b8ad653d5457250ab92a9d13fb8db4b
sagar jhode <sagar.jhode@iviewlabs.net>
2022-10-10 18:11:51 +0530

    code structure

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,88 @@
+import "./login.css";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+type Inputs = {
+    username: string;
+    password: string
+  };
+  const validationSchema = Yup.object()
+  .shape({
+    username: Yup.string().required()
+      .required("*Username is required")
+      .min(3, "*Username must be at least 3 characters")
+      .max(15, "*Username must not exceed 20 characters"),
+    password: Yup.string().required()
+      .required("*Password is required")
+      .min(6, "*Password must be at least 6 characters")
+      .max(40, "*Password must not exceed 40 characters"),
+  })
+  .required();
+
+const Login = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm<Inputs>({
+        resolver: yupResolver(validationSchema),
+      });
+
+      const onSubmit = (data:any) => {
+        const registerData:any = localStorage.getItem("register");
+        const parsedData = JSON.parse(registerData);
+        if(parsedData.username === data.username && parsedData.password === data.password){
+            alert("yes")
+        }else{
+            alert("no")
+        }
+      };
+    
+  return (
+    <div className="login-header-div">
+         <div className="login-form">
+            <h3 className="login-heading">Login</h3>
+             <hr/>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <input
+                type="text"
+                placeholder="Your Username"
+                {...register("username")}
+                className="input-fields-login"
+              />
+              <div className="invalid-feedback">{errors.username?.message}</div>
+
+              <br />
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Your Password"
+                {...register("password")}
+                className="input-fields-login"
+              />
+
+              <div className="invalid-feedback">{errors.password?.message}</div>
+              <br />
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="login-btn"
+              >
+                Login
+              </button>
+            </div>
+            <div className="register-sec">
+                <span className="forgott-pass">Forgott Password?</span><br/>
+                <span className="account-des-login">Don't have any account ? <span className="register-link">Register</span></span>
+            </div>
+          </form>
+        </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/sign-up/Signup.tsx
@@ -0,0 +1,136 @@
+import "./signup.css";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+type Inputs = {
+  username: string;
+  email: string;
+  mobile:string;
+  password: string;
+  confirmPassword: string;
+};
+const validationSchema = Yup.object()
+  .shape({
+    username: Yup.string().required()
+      .required("*Username is required")
+      .min(3, "*Username must be at least 3 characters")
+      .max(15, "*Username must not exceed 20 characters"),
+    email: Yup.string().required("*Email is required").email("*Email is invalid"),
+    mobile:Yup.string().required()
+    .required("*Mobile number is required")
+      .min(3, "*Mobile no. must be at least 10 characters")
+      .max(15, "*Mobile no. must not exceed 12 characters"),
+    password: Yup.string().required()
+      .required("*Password is required")
+      .min(6, "*Password must be at least 6 characters")
+      .max(40, "*Password must not exceed 40 characters"),
+    confirmPassword: Yup.string().required()
+      .required("*Confirm Password is required")
+      .oneOf([Yup.ref("password"), null], "*Confirm Password does not match"),
+    acceptTerms: Yup.bool().oneOf([true], "*Accept Terms is required"),
+  })
+  .required();
+
+const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = (data:any) => {
+    const register = {
+      username: data.username,
+      email: data.email,
+      mobile:data.mobile,
+      password: data.password,
+    };
+    localStorage.setItem("register", JSON.stringify(register));
+   console.log(register)
+  };
+
+  return (
+    <>
+      <div className="signup-header-div">
+        <div className="register-form">
+            <h3 className="register-heading">Register</h3>
+             <hr/>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <input
+                type="text"
+                placeholder="Your Username"
+                {...register("username")}
+                className="input-fields"
+              />
+              <div className="invalid-feedback">{errors.username?.message}</div>
+
+              <br />
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="Your Email"
+                {...register("email")}
+                className="input-fields"
+              />
+              <div className="invalid-feedback">{errors.email?.message}</div>
+
+              <br />
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="Your Mobile no."
+                {...register("mobile")}
+                className="input-fields"
+              />
+              <div className="invalid-feedback">{errors.mobile?.message}</div>
+
+              <br />
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Your Password"
+                {...register("password")}
+                className="input-fields"
+              />
+
+              <div className="invalid-feedback">{errors.password?.message}</div>
+              <br />
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                {...register("confirmPassword")}
+                className="input-fields"
+              />
+              <div className="invalid-feedback">
+                {errors.confirmPassword?.message}
+              </div>
+              <br />
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="signup-btn"
+              >
+                Register
+              </button>
+            </div>
+            <div className="login-sec">
+                <span className="account-des">Already have an account ? <span className="login-link">Login</span></span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
