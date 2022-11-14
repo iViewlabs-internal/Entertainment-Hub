0efffe1bf9addbdd5dc59565ced50096a44625ca
sagar jhode <sagar.jhode@iviewlabs.net>
2022-11-14 17:25:49 +0530

    working on redux

--- src/pages/movies/Movies.tsx
+++ src/pages/movies/Movies.tsx
@@ -145,3 +145,154 @@ const Movies = () => {
 };
 
 export default Movies;
+
+// import { Container,Grid,SelectChangeEvent } from "@mui/material";
+// import { useEffect, useState } from "react";
+// import Content from "../../components/content/Content";
+// import Footer from "../../components/footer/Footer";
+// import Language from "../../components/language/Language";
+// import Loader from "../../components/loader/Loader";
+// import Navbar from "../../components/navbar/Navbar";
+// import MyPagination from "../../components/pagination/MyPagination";
+// import "./movies.css";
+// import {useGetAllMoviesQuery } from "../../redux/services/entertainment";
+// import { useDispatch, useSelector } from "react-redux";
+// import { moviePage } from "../../redux/actions";
+
+// const mykey = process.env.REACT_APP_USER_API_KEY;
+// const Movies = () => {
+//   const responseInfo = useGetAllMoviesQuery();
+//   const dispatch = useDispatch()
+//   const page = useSelector((store) => store);
+//   console.log(page)
+//   console.log(responseInfo.data)
+//   console.log(responseInfo)
+
+//   // const [page, setPage] = useState(1);
+//   const [loading, setLoading] = useState(true);
+//   const [data, setData] = useState<any[]>([]);
+//   const [genre, setGenre] = useState<any[]>([]);
+//   const [genreArr, setGenreArr] = useState<any[]>([]);
+//   let [genreStr, setgenreStr] = useState("");
+//   const [numOfPages, setNumOfPages] = useState(0);
+
+//   const tempFunc = (id: number, idStr: string) => {
+//     const temp = document.querySelector(`#${idStr}`) as HTMLInputElement;
+
+//     if (temp.checked) {
+//       setGenreArr([...genreArr, id]);
+//     } else {
+//       // if (genreArr.includes(id) === true) {
+//       genreArr.splice(genreArr.indexOf(id), 1);
+//       // setGenreArr([...genreArr]);
+//       // }
+//     }
+//   };
+//   const applyFilter = () => {
+//     setgenreStr("");
+//     let tempStr = "";
+//     genreArr.map((val) => {
+//       tempStr += val + ",";
+//     });
+//     setgenreStr(tempStr);
+//     closeNav();
+//   };
+//   const [language, setLanguage] = useState('en-Us');
+
+//   const handleChange = (event: SelectChangeEvent) => {
+//     setLanguage(event.target.value as string);
+//   };
+
+//   useEffect(() => {
+//         setData(responseInfo?.data?.results);
+//         setNumOfPages(responseInfo?.data?.total_pages >= 500 ? 500 : responseInfo?.data?.total_pages);
+//         setLoading(false);
+//   }, [page, genreStr,language]);
+
+//   useEffect(() => {
+//     fetch(
+//       `https://api.themoviedb.org/3/genre/movie/list?api_key=${mykey}&language=en-US`
+//     )
+//       .then((res) => res.json())
+//       .then((items) => {
+//         setGenre(items.genres);
+//       });
+//   }, [page]);
+//   const openNav = () => {
+//     document.getElementById("mySidenav")!.style.width = "250px";
+//   };
+
+//   const closeNav = () => {
+//     document.getElementById("mySidenav")!.style.width = "0";
+//   };
+  
+//   return (
+//     <>
+//       <Navbar />
+//       <div id="mySidenav" className="sidenav">
+//         <div className="genres-div">
+//           <h4>Genres</h4>
+//           <a href="#!" className="closebtn" onClick={closeNav}>
+//             &times;
+//           </a>
+//         </div>
+//         <hr className="hr-genre" />
+//         {genre?.map((val) => {
+//           return (
+//             <div key={val.id} id="header-filter-div">
+//               <input
+//                 type="checkbox"
+//                 id={`check${val.id}`}
+//                 onClick={() => tempFunc(val.id, `check${val.id}`)}
+//               />
+//               <label className="all-lables">{val.name}</label>
+//             </div>
+//           );
+//         })}
+//         <button onClick={applyFilter} className="btn-apply">
+//           Apply Filters
+//         </button>
+//       </div>
+
+//       <div className="container-items">
+//         <Container>
+//           <div className="top-movies-div">
+//             <h4 className="filter-head" onClick={openNav}>
+//               Filter<i className="fa-solid fa-filter"></i>
+//             </h4>
+//             <h1 className="movie-head">Movies</h1>
+//             <Language language={language} handleChange={handleChange}/>
+//           </div>
+//           {loading ? (
+//             <Loader />
+//           ) : (
+//             <Grid container spacing={2} alignItems="stretch">
+//               {data &&
+//                 data?.map((val: any) => {
+//                   return (
+//                     <Content
+//                       key={val.id}
+//                       id={val.id}
+//                       title={val.title ? val.title : val.name}
+//                       release_date={val.release_date}
+//                       poster={val.poster_path}
+//                       mediaType="movie"
+//                       overview={val.overview}
+//                       vote_average={val.vote_average}
+//                     />
+//                   );
+//                 })}
+//             </Grid>
+//           )}
+//         </Container>
+//         {numOfPages > 1 && (
+//           <MyPagination setPage={page} numOfPages={numOfPages} />
+//         )}
+//           {(!data?.length && loading === false) ? <h1 className="no-data-heading"> <i className="fa-solid fa-face-frown"></i> No Data Found</h1> : ""}
+//       </div>
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default Movies;
---
+++ src/redux/actions/index.ts
@@ -0,0 +1,5 @@
+export const MOVIE_PAGE = 'MOVIE_PAGE';
+ export const moviePage = (payload: any) => ({
+        type : MOVIE_PAGE,
+        payload
+    })
